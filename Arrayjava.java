6f9ec3fce242f7bc93ddbf8e2bc6d257f661ec3a
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-04-29 22:31:13 +0530

    Create Arrayjava.java
    i started java from the begining and started the coding and logic building again

---
+++ Arrayjava.java
@@ -0,0 +1,211 @@
+package day5array;
+import java.util.Scanner;
+import java.util.Arrays;  // Import for Arrays.sort
+
+public class Arraypart {
+
+    public static void main(String[] args) {
+        
+ 	
+    		// ARRAY SORTING IN ASCENDING ORDER 
+    	
+    	  a[] = new int[5];
+        Scanner s1 = new Scanner(System.in);
+        
+        System.out.println("Accept Array:");
+        for (int i = 0; i < 5; i++) {
+            a[i] = s1.nextInt();
+        }
+
+        Arrays.sort(a); // This sorts the array in ascending order
+
+        System.out.println("Array in Ascending Order:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    	
+    	
+    	
+    	
+    	
+    	
+    			
+    		}
+    		
+    	}
+    	
+ 	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+//    	int a[] = new int[5];
+//    	
+//    	int temp= a[0];
+//         Scanner s1 = new Scanner(System.in);
+//         
+//         System.out.println("Accept Array:");
+//         for (int i = 0; i < 5; i++) {
+//             a[i] = s1.nextInt();  
+//             }
+//       
+//         
+
+
+
+    /* for (int i=0; i<5; i++) {
+         	 if(a[i]%2!=0) {
+	      	 temp=temp+a[i];
+	    }
+	
+	 
+	
+	 
+	 
+	 
+}
+
+
+System.out.println("sum of odd array number"+temp);
+
+ */
+         
+
+
+
+
+
+
+         
+         
+         
+         
+         
+         
+         
+         //PRINT MAX NUMBER   AND MIN NUMBER JUST HAVE TO CHNANGE SYMBOL AND CODE
+       /*  int Max=a[0];
+         for(int i=0;i<5;i++)
+         {
+        	 if(Max<a[i]) {
+        		 Max=a[i];
+        	 }
+         }
+         System.out.println("Print Max Number:"+Max);
+         
+       */  
+         
+         
+         
+         
+ 	
+    	
+    	
+    	// ARRAY SORTING IN ASCENDING ORDER 
+    	
+    	/*  a[] = new int[5];
+        Scanner s1 = new Scanner(System.in);
+        
+        System.out.println("Accept Array:");
+        for (int i = 0; i < 5; i++) {
+            a[i] = s1.nextInt();
+        }
+
+        Arrays.sort(a); // This sorts the array in ascending order
+
+        System.out.println("Array in Ascending Order:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+*/
+		
+		
+		
+		
+		
+		//SUM OF ELEMENT
+		
+		/*	int a[]= new int[5];
+		
+	   int b=0;
+	    
+	    Scanner s1 = new Scanner(System.in);
+	    System.out.println("accept Array :");
+	    for(int i=0;i<5;i++)
+	    {
+	    	a[i]=s1.nextInt();
+	    	b=b+a[i];
+	    	System.out.println("sum of array is:"+b);
+	    }
+	   */
+	   
+	    
+	    
+		
+		
+		
+		
+		//Display array
+		
+		/*  int a[]= new int[5];
+    
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("accept Array :");
+    for(int i=0;i<5;i++)
+    {
+    	a[i]=s1.nextInt();
+    }
+    System.out.println("display Araay !");
+    for(int i=0; i<a.length;i++)
+    {
+    	System.out.println(a[i]);
+    }
+    	
+    */
+    	
+    	
+    	
+//
+//int a[][]=new int[3][3];
+//Scanner s1= new Scanner(System.in);
+//
+//System.out.print("Enter the array ");
+//for(int i=0;i<3;i++) {
+//	
+//	for(int j=0;j<3;j++){
+//		
+//		a[i][j]=s1.nextInt();
+//		
+//		
+//if(a[i][j]%2==0){
+//	System.out.print(" 0 ");
+//
+//	
+//}
+//		
+//
+//System.out.print( a[i][j]+" ");
+//
+//
+
+    	
+    	
+	
+
+
