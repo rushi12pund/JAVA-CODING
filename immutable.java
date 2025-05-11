3417d4fa24802dd9c6f4b5e1e47202adedc19e8e
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-11 15:32:02 +0530

    Update immutable.java

--- immutable.java
+++ immutable.java
@@ -1,14 +1,24 @@
 package JAVACOnstructo;
 
 public class inhe {
-
+//immutable object : its creates another object  to print the concated veriable 
 	public static void main(String args[]) {
 	    String name= new String("hello");
 	    
-	   name=name.concat("haryaa");	 
+	   name=name.concat("india");	 
 	    
 	    System.out.println(name);
+
+
+		    
+	//mutable : here we can directly append the veriable to the hello
 	    
+	    StringBuilder b1= new StringBuilder("hello");
+	 b1= b1.append("india");
+	 
+	 System.out.println(b1);
+	 
+	 
 	}
 	
 	
