f47482fed8b85c19e8fd755d39db2a46f7ffd700
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-11 15:23:54 +0530

    Create immutable.java

---
+++ immutable.java
@@ -0,0 +1,15 @@
+package JAVACOnstructo;
+
+public class inhe {
+
+	public static void main(String args[]) {
+	    String name= new String("hello");
+	    
+	   name=name.concat("haryaa");	 
+	    
+	    System.out.println(name);
+	    
+	}
+	
+	
+}
