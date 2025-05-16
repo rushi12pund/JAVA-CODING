7c4ca09ef74add2e1dc58edd00503b2ed7c0b2e3
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 21:53:27 +0530

    Update loopingcontrolCODE4.java

--- loopingcontrolCODE4.java
+++ loopingcontrolCODE4.java
@@ -11,9 +11,13 @@ public class loop3 {
 	public static void main(String[] args) {
 		
 		for(int i=0;i<3;i++) {
-			System.out.print("*&");
+			System.out.print("*");
 			for(int j=0;j<1;j++) {
-				System.out.print("*&");
+				System.out.print("&");
+			}for(int j=0;j<1;j++) {
+				System.out.print("*");
+			}for(int j=0;j<1;j++) {
+				System.out.print("&");
 			}
 			System.out.println(" ");	
 			
