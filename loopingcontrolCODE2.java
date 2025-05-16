23a299dee941dee983204bd1d9dc0cd4217627d7
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 21:23:56 +0530

    Create loopingcontrolCODE2.java

---
+++ loopingcontrolCODE2.java
@@ -0,0 +1,27 @@
+package loop;
+
+//1111
+//2222
+//3333
+public class numberinloop {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<4;i++) {
+			System.out.print("1");
+			
+		}
+		System.out.println("");
+		
+		for(int j=0;j<2;j++) {
+			System.out.print("2");
+			for(int x=0;x<2;x++) {
+				System.out.print("2");
+				
+			}
+			System.out.println("2");
+		}
+		
+		
+	}
+	
+	}
