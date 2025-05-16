4eddc847be6c6561db5e9e29efe89f2da7b3b175
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 21:49:28 +0530

    Create loopingcontrolCODE4.java

---
+++ loopingcontrolCODE4.java
@@ -0,0 +1,27 @@
+
+//*&*&
+//*&*&
+//*&*&
+
+
+
+package loop;
+
+public class loop3 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<3;i++) {
+			System.out.print("*&");
+			for(int j=0;j<1;j++) {
+				System.out.print("*&");
+			}
+			System.out.println(" ");	
+			
+		}
+		
+		
+		
+		
+	}
+	
+	}
