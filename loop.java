c273d9ea1b900bff41867172015547e629b9970e
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 16:19:28 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,26 @@
+package loop;
+
+public class loops {
+
+	public static void main(String[] args) {
+		int temp=0;
+
+		for(int i=2;i<=10;i++) {
+			
+			
+			System.out.println(" "+i);
+			
+			
+			for(int j=2;j<=10;j++) {
+				
+				temp=i*j;
+				System.out.println(" "+temp);
+				
+			}
+		}
+		
+	
+		
+	}
+
+}
