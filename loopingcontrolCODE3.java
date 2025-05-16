f09891b6e5bc637f5fd02d0f52a4ee1b7c9b43b2
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 21:42:35 +0530

    Create loopingcontrolCODE3.java

---
+++ loopingcontrolCODE3.java
@@ -0,0 +1,25 @@
+package loop;
+
+//1357
+//1357
+//1357
+public class loop2 {
+
+	public static void main(String[] args) {
+		for( int i=0;i<3;i++) {
+			
+		for(int j=1;j<=7;j=j+2)	{
+			System.out.print(j);
+		}
+		 System.out.println("");
+		}
+	}
+
+}
+  
+
+
+
+
+
+
