d519ff892d89f3a36f0169a951afd6a52fd11495
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 22:14:23 +0530

    Create loopingcontrolCODE6.java

---
+++ loopingcontrolCODE6.java
@@ -0,0 +1,26 @@
+package loop;
+
+public class loop6 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<3;i++) {
+			System.out.print("a");
+			for(int j=0;j<1;j++) {
+				System.out.print("a");
+			}for(int j=0;j<1;j++) {
+				System.out.print("b");
+			}for(int j=0;j<1;j++) {
+				System.out.print("b");
+			}
+			System.out.println(" ");	
+        
+		
+		
+		}
+		
+		
+		
+	}
+
+}
