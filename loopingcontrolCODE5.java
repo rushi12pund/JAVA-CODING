dc8b3842edbee95b1a765266176428721f8e65e9
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 21:55:16 +0530

    Create loopingcontrolCODE5.java

---
+++ loopingcontrolCODE5.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class loop4 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++) {
+			System.out.print("A");
+			for(int j=0;j<1;j++) {
+				System.out.print("B");
+			}for(int j=0;j<1;j++) {
+				System.out.print("C");
+			}for(int j=0;j<1;j++) {
+				System.out.print("D");
+			}
+			System.out.println(" ");	
+			
+		}
+		
+	}
+
+}
