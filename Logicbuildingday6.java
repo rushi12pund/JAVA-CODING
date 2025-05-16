5e96d0b5ac006c1feefba6c01a5d6697167b3772
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-16 16:18:48 +0530

    Create Logicbuildingday6.java

---
+++ Logicbuildingday6.java
@@ -0,0 +1,23 @@
+package loop;
+
+//****
+//****
+//****
+public class printing {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i<3;i++) {
+			System.out.print("*");
+			for(int j=0;j<2;j++) {	
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+			}
+			
+		}
+		
+		
+		
+	
