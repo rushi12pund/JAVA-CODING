1aefcdf549dca66369708d80f77038bf8a81e628
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-21 15:46:18 +0530

    Create loopingcontrolCODE7.java

---
+++ loopingcontrolCODE7.java
@@ -0,0 +1,26 @@
+package practicaldaythuslooping;
+
+//**
+//*  *
+//*  *
+//**
+
+public class loops {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i<2;i++) {
+			System.out.print("*");
+			
+		}System.out.println();
+      for(int j=0;j<2;j++) {
+    	  System.out.println("*"+"  "+"*");
+      }
+      
+      for(int i=0;i<2;i++) {
+			System.out.print("*");
+			
+		}
+	}
+
+}
