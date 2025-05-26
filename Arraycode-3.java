625e8575a407e9f7cfd04854803cf503e345c2d8
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-26 17:01:56 +0530

    Create Arraycode-3.java

---
+++ Arraycode-3.java
@@ -0,0 +1,14 @@
+package arrayproj1;
+
+public class index {
+	public static void main(String[] args) {
+		
+		int arr[]= {45,86,95,67,45,89,45};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("index of 3 is "+arr[3]);	
+			break;
+	}
+	
+	
+	}
+}
