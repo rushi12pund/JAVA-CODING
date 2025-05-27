05ec60ab2e0bbbffceca9c0ad2eed9c1132017a7
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-05-27 16:59:41 +0530

    Create Arraycode-5.java

---
+++ Arraycode-5.java
@@ -0,0 +1,20 @@
+package arrayproj1;
+// find the specific element present is array
+public class specificelemen {
+	public static void main(String[] args) {
+		int arr[]= {1,4,5,6,2,1,5,2};
+	    int a=6;
+		for(int i=0;i<arr.length;i++) {
+		if(arr[i]==a) {
+			System.out.println("this element present in index"+ i);
+		}
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+}}
